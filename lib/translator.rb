23a1a92378d9f54c15df0fee9a85f1cffdc4b908
Claudia Borghini <claborghini@gmail.com>
2020-07-07 14:23:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,17 +4,15 @@
 def load_library(file_path)
  emoticons = YAML.load_file(file_path)
  translator = {}
-
  emoticons.each do |mood, value_array|
  translator[mood] = {:english => value_array[0], :japanese => value_array[1]}
  end
  translator
-#binding.pry
 end
 
 def get_japanese_emoticon(file_path, english_emoji)
   library = load_library(file_path)
-  library.each 
+  library.each
   # code goes here
 end
 
