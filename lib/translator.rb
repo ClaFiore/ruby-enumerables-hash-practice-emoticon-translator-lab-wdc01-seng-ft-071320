cc9ba6579f1ba6cf64a95e5f490b085550295d85
Claudia Borghini <claborghini@gmail.com>
2020-07-07 15:33:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,9 +17,7 @@ def get_japanese_emoticon(file_path, eng_emoticon)
 
   library.each do |key, values|
       if key[:english] == eng_emoticon
-
          return key[:japanese]
-
       else
         puts "Sorry, that emoticon was not found"
       end
