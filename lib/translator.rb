18fbd027d84dcde86abea11d0fd434cd7abe0e0f
Claudia Borghini <claborghini@gmail.com>
2020-07-07 14:55:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_english_meaning(file_path, jap_emoticon)
   library = load_library(file_path)
 
       library.each do |key, values|
+        binding.pry
       if library[:key][:japanese] == emoticon
       return library[:key][:english]
       else
